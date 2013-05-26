c8ec9ebc87a65eab17d33a7809144a2a486a5e2b
MRSC <cloud760607@hotmail.com>
2013-05-26 18:57:02 +0000

    bug fixed

--- mail.php
+++ mail.php
@@ -1,8 +1,8 @@
 <?php
 require_once("phpmailer/class.phpmailer.php");
-class MailLib
+class mail
 {
-    public static function send_mail($user_email, $user_name, $subject, $body)
+    public static function sendmail($user_email, $user_name, $subject, $body)
     {
         $mail = new PHPMail();
         $mail->IsSMTP();
@@ -12,14 +12,14 @@ class MailLib
         $mail->Port = 465;
         $mail->CharSet = "utf-8";
 
-        $mail->Username = "service@eatgo.com";
-        $mail->Password = "pmlalala";
+        $mail->Username = "xxxxxx@xxxxxx";
+        $mail->Password = "ur password";
 
         //$mail->AddAttachment("");
     
         $mail->Subject = $subject;
-        $mail->From = "service@eatgo.com";
-        $mail->FromName = "走吧 客服中心";
+        $mail->From = "xxxxxx@xxxxxx";
+        $mail->FromName = "xxxxxxxxx";
         $mail->Body = $body;
         $mail->IsHTML(true);
         $mail->AddAddress($user_email, $user_name);
